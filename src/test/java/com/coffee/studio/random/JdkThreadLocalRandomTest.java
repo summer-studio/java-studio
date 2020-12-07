b5db7aedc1de9d8d2ec59c7272d4a703bb276410
bin <mango2049@163.com>
2020-12-07 11:40:07 +0800

    init

--- src/test/java/com/coffee/studio/random/JdkThreadLocalRandomTest.java
+++ src/test/java/com/coffee/studio/random/JdkThreadLocalRandomTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 public class JdkThreadLocalRandomTest {
 
-    JdkThreadLocalRandom jdkThreadLocalRandom = new JdkThreadLocalRandom();
+    private JdkThreadLocalRandom jdkThreadLocalRandom = new JdkThreadLocalRandom();
 
     @Test
     public void getRandom() {
